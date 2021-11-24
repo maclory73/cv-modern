731168cfa3dbc9cd73ffd2e28104275bca3f306d
maclory73 <lory.carvajol997@gmail.com>
2021-11-24 13:29:05 +0100

    refresh

--- src/components/Knowledges/Experiences.js
+++ src/components/Knowledges/Experiences.js
@@ -8,45 +8,45 @@ const Experiences = () => {
                <h4>Formation développeur web et appli mobile 2020-2021</h4>
                
                <p>
-                Maquetter une application.
+                -Maquetter une application.
                </p>
                <p>
-                   Développer les composants d'accès aux données.
+                   -Développer les composants d'accès aux données.
                </p>
                <p>
-                   Concevoir des interfaces web statique et dynamique responsive.
+                   -Concevoir des interfaces web statique et dynamique responsive.
                </p>
                <p>
-                   Développer la partie back-end d'une application web.
+                   -Développer la partie back-end d'une application web.
                </p>
                <p>
-                   Concevoir une interface utilisateur avec wordpress.
+                   -Concevoir une interface utilisateur avec wordpress.
                </p>
            </div>      
            <div className="exp-2">
                <h4>Evaluateur de moteur de recherche 2018-2020</h4>
                
                 <p>
-                    Projet de comparaison de pages de moteur de recherche..
+                    -Projet de comparaison de pages de moteur de recherche..
                 </p>
                 <p>
-                    Commenter des jugements en Anglais d'après une guideline complexe.
+                    -Commenter des jugements en Anglais d'après une guideline complexe.
                 </p>
                 <p>
-                    Suivre la logique principale du guideline avec des exemples répétitifs.
+                    -Suivre la logique principale du guideline avec des exemples répétitifs.
                </p>
            </div>
            <div className="exp-3">
                <h4>Technicien spécialisé pour Alstom-Areva 2001-2009</h4>
                
                <p>
-                Montage de sous-ensembles mécanique en "atelier blanc".
+                -Montage de sous-ensembles mécanique en "atelier blanc".
             </p>
             <p>
-                Montage des sous-ensembles sur site en France sous la direction du superviseur. 
+                -Montage des sous-ensembles sur site en France sous la direction du superviseur. 
             </p>
             <p>
-                Capacité à travailler proprement en sécurité en atelier comme en hauteur.
+                -Capacité à travailler proprement en sécurité en atelier comme en hauteur.
             </p>
            
            </div>

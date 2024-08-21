c927e2642c55f4f0d34cf8861afe37776913561c
Carvajol lory <66648964+maclory73@users.noreply.github.com>
2024-08-21 22:58:29 +0200

    refresh

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import React from 'react';
-import { BrowserRouter , Route, Switch } from 'react-router-dom'
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Knowledges from './pages/Knowledges';
 import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
-import NotFound  from 'http-errors';
-
+import NotFound from './pages/NotFound';
+import Modal from './components/Modal';
 
 const App = () => {
+  const [showModal, setShowModal] = useState(false);
+  const [modalTitle, setModalTitle] = useState('');
+  const [modalContent, setModalContent] = useState(null);
+
+  const openModal = (title, content) => {
+    setModalTitle(title);
+    setModalContent(content);
+    setShowModal(true);
+  };
+
+  const closeModal = () => {
+    setShowModal(false);
+    setModalTitle('');
+    setModalContent(null);
+  };
+
   return (
-    <>
-      <BrowserRouter>
-        <Switch>
-          <Route path="/" exact component={Home} />
-          <Route path="/competences" component={Knowledges} />
-          <Route path="/contact" component={Contact} />
-          <Route path="/portfolio" component={Portfolio} />
-          <Route  component={NotFound} /> 
-        </Switch>     
-      </BrowserRouter>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home openModal={openModal} />} />
+        <Route path="/competences" element={<Knowledges />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Modal show={showModal} onClose={closeModal} title={modalTitle}>
+        {modalContent}
+      </Modal>
+    </Router>
   );
 };
 
